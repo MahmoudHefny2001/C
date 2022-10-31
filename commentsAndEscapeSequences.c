8788766a79334b24040f1de12b82fa4325dd3238
MahmoudHefny2001 <hefny4@gmail.com>
2022-10-31 23:20:02 +0200

    message

--- commentsAndEscapeSequences.c
+++ commentsAndEscapeSequences.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     // This is a comment
+    /*
+    This
+    is
+    a
+    multiline
+    comment
+    */
     printf("I like pizza\n");
+    printf("\n");
+
+    /*
+        escape sequence = character combination consisting of a backslash \
+        followed by a letter or combination of digits.
+        They specify a line or string of text.
+        \n = new line
+        \t = tab
+    */
+    printf("This\nis\na\nnew\nline\nescape\nsequence\nprinting\n");
+
+    printf("\n1\t2\t3\n4\t5\t6\n7\t8\t9\n\n"); 
+
+    printf("\"This is my quote\" -- \'Mahmoud Hefny\'\n");
+
     return 0;
 }
