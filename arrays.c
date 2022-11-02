dcf1249e216fe45643bc383dc17f89f84dcc5350
MahmoudHefny2001 <hefny4@gmail.com>
2022-11-02 23:08:51 +0200

    message

---
+++ arrays.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    double prices[] = {5.0, 6, 1.5, 3.25, 4.75, 8.0};
+    
+    printf("%lf\n", prices[3]);
+
+    return 0;
+}
--- findMax.c
+++ findMax.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 double findMax(double number1, double number2){
-    if (number1 > number2){
-        return number1;
-    }
-    return number2;
+    // if (number1 > number2){
+    //     return number1;
+    // }
+    // return number2;
+
+    return (number1 > number2) ? number1 : number2 ;
 }
 
 int main(){
 
-    double max = findMax(2, 6);
+    double max = findMax(20, 80);
 
     printf("Maximum is: %lf\n", max);
 
